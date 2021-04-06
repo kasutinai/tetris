46c63ad63c2540dacac5b886ef9312033f709400
kasutinai <ak9mail@mail.ru>
2021-04-06 21:47:28 +0700

    3.5.3

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -9,13 +9,22 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
+            Figure[] f = new Figure[2];
+            f[0] = new Square(2, 5, '*');
+            f[1] = new Stick(4, 5, '#');
+
+            foreach (Figure fig in f)
+            {
+                fig.Draw();
+            }
+
             /*Square s = new Square(2, 5, '*');
-            s.Draw();*/
+            s.Draw();
 
             Stick s = new Stick(2, 5, '*');
             s.Draw();
 
-            /*Point p1 = new Point(2, 3, '*');
+            Point p1 = new Point(2, 3, '*');
             
             p1.Draw();*/
 
